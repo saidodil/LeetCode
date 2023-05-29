9f8cc1e78e3fbd36d111d98afb9d6341e7a95776
Dilnoza Saidova <73456940+saidodil@users.noreply.github.com>
2023-05-29 16:25:47 -0700

    Update and rename 1805. Number of Different Integers in a String to NumberOfDifferentIntegersInAString.java

--- NumberOfDifferentIntegersInAString.java
+++ NumberOfDifferentIntegersInAString.java
@@ -1,4 +1,15 @@
-class Solution {
+/**
+ * 1805. Number of Different Integers in a String
+ * 
+ * You are given a string word that consists of digits and lowercase English letters.
+ * You will replace every non-digit character with a space. 
+ * For example, "a123bc34d8ef34" will become " 123  34 8  34". 
+ * Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
+ * Return the number of different integers after performing the replacement operations on word.
+ * Two integers are considered different if their decimal representations without any leading zeros are different.
+ */
+
+class NumberOfDifferentIntegersInAString {
   public int numDifferentIntegers(String theWord) {
       Set<String> distinctIntegers = new HashSet<>();
       StringBuilder currentInteger = new StringBuilder();
@@ -20,9 +31,8 @@ class Solution {
 
   private String removeLeadingZeros(StringBuilder theNum) {
       int start = 0;
-      while (start < theNum.length() - 1 && theNum.charAt(start) == '0') {
+      while (start < theNum.length() - 1 && theNum.charAt(start) == '0')
           start++;
-      }
       return theNum.substring(start);
   }
 }
