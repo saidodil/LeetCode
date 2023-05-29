222562f998eb576112a596c4e331406dd85ffaac
Dilnoza Saidova <73456940+saidodil@users.noreply.github.com>
2023-05-29 16:27:50 -0700

    Update and rename 1492. The kth Factor of n to TheKthFactorOfN.java

--- TheKthFactorOfN.java
+++ TheKthFactorOfN.java
@@ -1,4 +1,12 @@
-class Solution {
+/**
+ * 1492. The kth Factor of n
+ * 
+ * You are given two positive integers n and k.
+ * A factor of an integer n is defined as an integer i where n % i == 0.
+ * Consider a list of all factors of n sorted in ascending order, return the kth factor in this list or return -1 if n has less than k factors.
+ */
+
+class TheKthFactorOfN {
   public int kthFactor(int n, int k) {
       int count = 0;
       for (int i = 1; i * i <= n; i++) {
@@ -16,9 +24,8 @@ class Solution {
       for (int i = (int) Math.sqrt(n); i >= 1; i--) {
           if (n % i == 0) {
               count++;
-              if (count == k) {
+              if (count == k)
                   return n / i;
-              }
           }
       }
       return -1;
