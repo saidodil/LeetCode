3a0b34299117aaa6d5fc74bcb7ee8df50635d341
Dilnoza Saidova <73456940+saidodil@users.noreply.github.com>
2023-05-29 17:26:32 -0700

    766. Toeplitz Matrix

--- ToeplitzMatrix.java
+++ ToeplitzMatrix.java
@@ -10,7 +10,6 @@ class ToeplitzMatrix {
     public boolean isToeplitzMatrix(int[][] theMatrix) {
         int rows = theMatrix.length;
         int cols = theMatrix[0].length;
-
         for (int i = 1; i < rows; i++) {
             for (int j = 1; j < cols; j++) {
                 if (theMatrix[i][j] != theMatrix[i - 1][j - 1])
