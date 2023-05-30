95340125a925f38579bab42689fcbbed081e1c06
Dilnoza Saidova <73456940+saidodil@users.noreply.github.com>
2023-05-29 17:21:45 -0700

    1758. Minimum Changes To Make Alternating Binary String

--- MinimumChangesToMakeAlternatingBinaryString.java
+++ MinimumChangesToMakeAlternatingBinaryString.java
@@ -9,21 +9,21 @@
  */
 
 class MinimumChangesToMakeAlternatingBinaryString {
-  public int minOperations(String theS) {
-      // Number of changes needed if starting with '1'
-      int count1 = 0; 
-      // Number of changes needed if starting with '0'
-      int count0 = 0; 
+    public int minOperations(String theS) {
+        // Number of changes needed if starting with '1'
+        int count1 = 0; 
+        // Number of changes needed if starting with '0'
+        int count0 = 0; 
 
-      for (int i = 0; i < theS.length(); i++) {
-          // Check if the current position should have '1'
-          if (theS.charAt(i) != (i % 2 == 0 ? '1' : '0'))
-              count1++;
+        for (int i = 0; i < theS.length(); i++) {
+            // Check if the current position should have '1'
+            if (theS.charAt(i) != (i % 2 == 0 ? '1' : '0'))
+                count1++;
 
-          // Check if the current position should have '0'
-          if (theS.charAt(i) != (i % 2 == 0 ? '0' : '1'))
-              count0++;
-        
-      return Math.min(count1, count0);
-  }
+            // Check if the current position should have '0'
+            if (theS.charAt(i) != (i % 2 == 0 ? '0' : '1'))
+                count0++;
+
+        return Math.min(count1, count0);
+    }
 }
