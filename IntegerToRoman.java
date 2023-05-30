ae5540c2954d4cae301d936e942d10fa0a57b455
Dilnoza Saidova <73456940+saidodil@users.noreply.github.com>
2023-05-29 17:49:45 -0700

    12. Integer to Roman

--- IntegerToRoman.java
+++ IntegerToRoman.java
@@ -24,7 +24,7 @@
  * Given an integer, convert it to a roman numeral.
  */
 
-class Solution {
+class IntegerToRoman {
     public String intToRoman(int theNum) {
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
