264a2caec15c520a76ff92aa55f85bbfe9457a9f
Dilnoza Saidova <73456940+saidodil@users.noreply.github.com>
2023-05-29 17:15:57 -0700

    1507. Reformat Date

--- ReformatDate.java
+++ ReformatDate.java
@@ -12,32 +12,32 @@
  */
 
 class ReformatDate {
-  public String reformatDate(String theDate) {
-      String[] parts = theDate.split(" ");
+    public String reformatDate(String theDate) {
+        String[] parts = theDate.split(" ");
 
-      String day = parts[0].substring(0, parts[0].length() - 2);
-      String month = parts[1];
-      String year = parts[2];
+        String day = parts[0].substring(0, parts[0].length() - 2);
+        String month = parts[1];
+        String year = parts[2];
 
-      // Map month names to month numbers
-      Map<String, String> monthMap = new HashMap<>();
-      monthMap.put("Jan", "01");
-      monthMap.put("Feb", "02");
-      monthMap.put("Mar", "03");
-      monthMap.put("Apr", "04");
-      monthMap.put("May", "05");
-      monthMap.put("Jun", "06");
-      monthMap.put("Jul", "07");
-      monthMap.put("Aug", "08");
-      monthMap.put("Sep", "09");
-      monthMap.put("Oct", "10");
-      monthMap.put("Nov", "11");
-      monthMap.put("Dec", "12");
+        // Map month names to month numbers
+        Map<String, String> monthMap = new HashMap<>();
+        monthMap.put("Jan", "01");
+        monthMap.put("Feb", "02");
+        monthMap.put("Mar", "03");
+        monthMap.put("Apr", "04");
+        monthMap.put("May", "05");
+        monthMap.put("Jun", "06");
+        monthMap.put("Jul", "07");
+        monthMap.put("Aug", "08");
+        monthMap.put("Sep", "09");
+        monthMap.put("Oct", "10");
+        monthMap.put("Nov", "11");
+        monthMap.put("Dec", "12");
 
-      // Format day with leading zero if necessary
-      if (day.length() == 1)
-          day = "0" + day;
+        // Format day with leading zero if necessary
+        if (day.length() == 1)
+            day = "0" + day;
 
-      return year + "-" + monthMap.get(month) + "-" + day;
-  }
+        return year + "-" + monthMap.get(month) + "-" + day;
+    }
 }
